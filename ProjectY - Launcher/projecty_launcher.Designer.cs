6dff3f730a47f0ec98232d3dc1cd6eb05add1b29
LunaticK79 <81393071+LunaticK79@users.noreply.github.com>
2021-04-20 02:33:47 +0100

    Um launcher

---
+++ ProjectY - Launcher/projecty_launcher.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ProjectY___Launcher
+{
+    partial class AlterEGO
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AlterEGO));
+            this.Logotipo_PY = new System.Windows.Forms.PictureBox();
+            this.py_jogar = new System.Windows.Forms.PictureBox();
+            this.py_tokovoip = new System.Windows.Forms.PictureBox();
+            this.py_nomesv = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Logotipo_PY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.py_jogar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.py_tokovoip)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.py_nomesv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Logotipo_PY
+            // 
+            resources.ApplyResources(this.Logotipo_PY, "Logotipo_PY");
+            this.Logotipo_PY.BackColor = System.Drawing.Color.Transparent;
+            this.Logotipo_PY.Name = "Logotipo_PY";
+            this.Logotipo_PY.TabStop = false;
+            this.Logotipo_PY.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // py_jogar
+            // 
+            resources.ApplyResources(this.py_jogar, "py_jogar");
+            this.py_jogar.BackColor = System.Drawing.Color.Transparent;
+            this.py_jogar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.py_jogar.Name = "py_jogar";
+            this.py_jogar.TabStop = false;
+            this.py_jogar.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // py_tokovoip
+            // 
+            resources.ApplyResources(this.py_tokovoip, "py_tokovoip");
+            this.py_tokovoip.BackColor = System.Drawing.Color.Transparent;
+            this.py_tokovoip.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.py_tokovoip.Name = "py_tokovoip";
+            this.py_tokovoip.TabStop = false;
+            this.py_tokovoip.Click += new System.EventHandler(this.py_tokovoip_Click);
+            // 
+            // py_nomesv
+            // 
+            resources.ApplyResources(this.py_nomesv, "py_nomesv");
+            this.py_nomesv.BackColor = System.Drawing.Color.Transparent;
+            this.py_nomesv.Name = "py_nomesv";
+            this.py_nomesv.TabStop = false;
+            // 
+            // AlterEGO
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.Controls.Add(this.py_nomesv);
+            this.Controls.Add(this.py_tokovoip);
+            this.Controls.Add(this.py_jogar);
+            this.Controls.Add(this.Logotipo_PY);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "AlterEGO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Logotipo_PY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.py_jogar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.py_tokovoip)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.py_nomesv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Logotipo_PY;
+        private System.Windows.Forms.PictureBox py_jogar;
+        private System.Windows.Forms.PictureBox py_tokovoip;
+        private System.Windows.Forms.PictureBox py_nomesv;
+    }
+}
+
---
+++ ProjectY - Launcher/projecty_launcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectY___Launcher
+{
+    public partial class AlterEGO : Form
+    {
+        public AlterEGO()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("fivem://connect/45.146.255.136:30120");
+        }
+
+        private void py_tokovoip_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("ts3server://92.42.44.150/?port=9028&password=lunny1337");
+        }
+
+        private void py_patreon_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.patreon.com/carpediemrp");
+        }
+    }
+}
